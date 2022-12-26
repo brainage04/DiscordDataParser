909ceacb90d0cb8d10d38c190c0bd763e83cc28d
brainage04 <thomasmcmah21@gmail.com>
2022-12-27 03:38:18 +1000

    switched to PapaParse for CSV to JSON conversion

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 3. Unzip your Discord data and drag + drop the files into the repository folder. If you have not requested your Discord data, you can do so in the Privacy & Safety tab in User Settings.
 4. Press Ctrl + ` to open a new terminal window and type "npm install" to install dependencies.
 5. Type "node ." to parse your Discord data (just messages for now).
-6. If you wish to download all your attachments, you can do so by typing "node .\download_attachments.js". Increment the counter by 100 each time until you have downloaded all of your attachments.
+6. If you wish to download all your attachments, you can do so by typing "node .\download_attachments.js". Increment the counter by 100 each time until you have downloaded all of your attachments - trying to download all attachments at once may result in undocumented network behaviour.
+
+# Known Issues
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs-extra');
-const path = require('path');
+const Papa = require('papaparse');
 
 fs.ensureDirSync('./processed_package/private_messages/attachments');
 fs.ensureDirSync('./processed_package/private_messages/messages');
@@ -8,9 +8,6 @@ fs.ensureDirSync('./processed_package/public_messages/attachments');
 fs.ensureDirSync('./processed_package/public_messages/messages');
 
 const messageIndex = require('./package/messages/index.json');
-
-let messageIndexCounter = 0;
-let messageIndexCounterLimit = 1000;
 
 let privateMessagesContainer = {};
 let privateDeletedUsersContainer = {};
@@ -28,26 +25,21 @@ const replaceList = [
 function parseDiscordPackage() {
 	for (index in messageIndex) {
 		if (messageIndex[index] === null) {
-			continue;
-		} else if (messageIndex[index].length < 20) {
-			continue;
-		} else if (messageIndexCounter > messageIndexCounterLimit) {
-			continue;
+			messageIndex[index] = index;
 		}
-		
-		let discordMessages = fs.readFileSync(`./package/messages/c${index}/messages.csv`, { encoding: 'utf8', flag: 'r' });
-		discordMessages = discordMessages.split('\n').slice(1,-1); // remove 'ID,Timestamp,Contents,Attachments\r\n'
 
-		if (messageIndex[index].slice(0, 20) === 'Direct Message with ') { // parse private message channel contents
+		let discordMessages = fs.readFileSync(`./package/messages/c${index}/messages.csv`, { encoding: 'utf8', flag: 'r' });
+		discordMessages = discordMessages.split('\n').slice(1,-1).join('\n'); // remove 'ID,Timestamp,Contents,Attachments\r\n'
+		discordMessages = Papa.parse(discordMessages, {
+			dynamicTyping: true,
+			encoding: 'utf-8'
+		});
+		discordMessages = discordMessages.data;
+
+		if (messageIndex[index].includes('Direct Message with ')) { // parse private message channel contents
 			for (subindex in discordMessages) {
-				privateMessageArguments = discordMessages[subindex].split(',');
-				discordMessages[subindex] = {}; // convert array to object so variables can be assigned with names
-				discordMessages[subindex].id = privateMessageArguments[0];
-				discordMessages[subindex].timestamp = privateMessageArguments[1];
-				discordMessages[subindex].contents = privateMessageArguments[2];
-				discordMessages[subindex].attachments = privateMessageArguments[3];
-				if (privateMessageArguments[3] && privateMessageArguments[3].slice(0, 39) === "https://cdn.discordapp.com/attachments/") {
-					privateAttachmentsArray.push(privateMessageArguments[3]);
+				if (discordMessages[subindex][3] !== null) { // if message contains attachments
+					privateAttachmentsArray.push(discordMessages[subindex][3]);
 				}
 			}
 	
@@ -62,27 +54,21 @@ function parseDiscordPackage() {
 	
 			privateMessagesContainer[privateMessageObjectTag] = privateMessageObject; // put objects in array container
 	
-			if (privateMessageObjectTag.slice(0,13) === 'Deleted User ') { // filter deleted users
+			if (privateMessageObjectTag.includes('Deleted User ')) { // filter deleted users
 				privateDeletedUsersContainer[privateMessageObjectTag] = privateMessageObject; // put objects in array container
 			}
 	
 			fs.writeFileSync(`./processed_package/private_messages/messages/${privateMessageObjectTag}.json`, JSON.stringify(privateMessageObject, null, '\t'));
 		} else { // parse public message channel contents
 			for (subindex in discordMessages) {
-				publicMessageArguments = discordMessages[subindex].split(',');
-				discordMessages[subindex] = {}; // convert array to object so variables can be assigned with names
-				discordMessages[subindex].id = publicMessageArguments[0];
-				discordMessages[subindex].timestamp = publicMessageArguments[1];
-				discordMessages[subindex].contents = publicMessageArguments[2];
-				discordMessages[subindex].attachments = publicMessageArguments[3];
-				if (publicMessageArguments[3] && publicMessageArguments[3].slice(0, 39) === "https://cdn.discordapp.com/attachments/") {
-					publicAttachmentsArray.push(publicMessageArguments[3]);
+				if (discordMessages[subindex][3] !== null) { // if message contains attachments
+					publicAttachmentsArray.push(discordMessages[subindex][3]);
 				}
 			}
 	
 			let publicMessageObjectTag = messageIndex[index]; // get name of discord channel
 			for (index in replaceList) {
-				publicMessageObjectTag = publicMessageObjectTag.replace(replaceList[index][0], replaceList[index][1]); // tag validation for file names
+				publicMessageObjectTag = publicMessageObjectTag.replace(replaceList[index][0], replaceList[index][1]); // name validation for file names
 			}
 	
 			let publicMessageObject = {
@@ -93,8 +79,6 @@ function parseDiscordPackage() {
 	
 			fs.writeFileSync(`./processed_package/public_messages/messages/${publicMessageObjectTag}.json`, JSON.stringify(publicMessageObject, null, '\t'));
 		}
-	
-		messageIndexCounter++;
 	}
 
 	fs.writeFileSync('./processed_package/private_messages/_attachments.json', JSON.stringify(privateAttachmentsArray, null, '\t'));
