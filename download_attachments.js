714c8cbd4e4bfb7522a0f54be66b7bef5862dd09
brainage04 <thomasmcmah21@gmail.com>
2022-12-27 03:45:49 +1000

    fixed attachment downloader script

--- download_attachments.js
+++ download_attachments.js
@@ -17,20 +17,20 @@ async function downloadFile(url, filepath) {
 const privateAttachmentsArray = require('./processed_package/private_messages/_attachments.json');
 const publicAttachmentsArray = require('./processed_package/public_messages/_attachments.json');
 
-const firstAttachment = 0; // increment this by 100 at a time
+const firstAttachment = 200; // increment this by 100 at a time
 
-function downloadAttachmentBatch(attachmentsArray) {
+function downloadAttachmentBatch(attachmentsArray, attachmentsFolder) {
 	for (let index = firstAttachment; index < firstAttachment + 100; index++) {
 		const attachments = attachmentsArray[index].split(' ');
 	
 		for (let subindex = 0; subindex < attachments.length; subindex++) {
 			const fileName = `${index + 1}_${subindex + 1}_${attachments[subindex].split('/').slice(-1)}` // get file name and extension (found after last slash in URL)
 	
-			downloadFile(attachments[subindex], `./processed_package/attachments/${fileName}`);
+			downloadFile(attachments[subindex], `./processed_package/${attachmentsFolder}_messages/attachments/${fileName}`);
 		
 			console.log(`Downloaded attachment ${index + 1}/${attachmentsArray.length}: ${fileName}`);
 		}
 	}
 }
 
-downloadAttachmentBatch(privateAttachmentsArray);
+downloadAttachmentBatch(publicAttachmentsArray, "public");
